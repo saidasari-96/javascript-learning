56747c7ba471b4435e808d4b2cf3a98facfd32f0
saidasari-96 <sai96.dasari@gmail.com>
2024-09-06 11:08:24 +0530

    interview notes

--- bavaLearning.js
+++ bavaLearning.js
@@ -60,3 +60,69 @@ console.log(temp());
 // ! String iterations
 // ! array iterations
 // !
+
+
+// My name saikumar i am from andhra pradesh i have total 3.6 years of experience in frontend development i closely worked with vuejs, html, css, and javascript these are my complete skil set.coming to projects that i have worked in my recent company Photon is WSI DP to MFE migration and client is william sonama it is a US based ecomerece company this project is completely migration from java based aplication to vue.js application and my role is Vuejs developer.
+
+// let me explain breif about my recent project with roles and resbolities WSI DP to MFE migration is my recent project and client is william sonama it is a US based ecomerece company this project is completely migration from java based aplication to vue.js application
+
+// Roles & resbolities:
+
+// Involved in end-to-end development from requirement analysis to designing, coding, testing, product integration and debugging.
+
+// Developed and implemented reusable and efficient Vue components, Built with consistent and maintainable user interfaces across multiple modules in project
+// •	Integrated frontend vue application with the application bussiness layer. 
+// •	Implemented enhancements and fixed UAT and production bugs
+// ==============================================================
+
+// During the initial stage of the project, after two sprints the client revised the requirements, so by the time we had implemented the necessary components and a few functionalities, we had reestimated the requirements and explained them to the client, but the client had provided limited time, so we also spent extra time on weekends to meet the deadline.
+
+// projects:
+
+// WSI- DP to MFE Migration (11/2023 - 02/2024)
+// Nice-Market Insights (08/2022 - 08/2023)
+// Nice-Installer and UI Refresh (06/2021 - 07/2022)
+// Network Monitoring System (07/2018 - 06/2020)
+
+// ==========================================================
+
+
+// The :nth-child selector in CSS allows us to select and style a specific element based on its position in the parent container.
+// for example : a list of li elements inside a ul container to style the list items all the odd elements have a different background color from the even elements. we can use the :nth-child selector to achieve this effect.
+// ========================================================================
+// Relative positioning adjusts an element's position relative to its normal flow within the document.
+
+// Absolute positioning positions an element relative to its nearest positioned ancestor or the initial containing block and removes it from the document flow.
+
+// fixed: This is similar to absolute except here the elements are positioned relative to the <html> element.
+
+// =========================================================================
+// A CSS pseudo-element is used to style specified parts of an element.
+
+// For example, it can be used to:
+
+// Style the first letter, or line, of an element
+// Insert content before, or after, the content of an element
+// =====================================================================
+// A pseudo-class is used to define a special state of an element.
+
+// For example, it can be used to:
+
+// Style an element when a user mouses over it
+// Style visited and unvisited links differently
+// Style an element when it gets focus
+
+// key attribute Uniquely identifies each element within a v-for loop.
+// The key value should be unique within the current list iteration. This ensures Vue can correctly identify individual elements.
+
+// ========================================
+
+
+// I joined my father's business after my 1st company Unfortunately, it did not go well due to Corona pandemic and we faced more financial difficulties, so I restarted my IT career.
+
+
+// I joined the  Valuefy before joining in Xoriant, i was not assigned any project. Meanwhile, I've got a Xoriant Opportunity, So i immediately joined.
+// I asked for early release from valuefy, but they didn't give,
+
+
+// How is the carrier growth
